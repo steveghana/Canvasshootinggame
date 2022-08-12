57765d9dc947f2e9cc1ff25d0f158abe1cac8362
unknown <boatengstephen707@gmail.com>
2022-08-12 11:45:11 -0800

    game completed

--- app.js
+++ app.js
@@ -1,187 +1,336 @@
-let canvas = document.querySelector('canvas')
-let c = canvas.getContext('2d')
-canvas.width = innerWidth
-canvas.height = innerHeight
+// import gsap from "./gsap-public (1)/gsap-public/src/gsap-core"
 
-class Player{
-    constructor(x, y, radius, color){
-this.x = x
-this.y = y
-this.radius = radius
-this.color = color
-    }
-    draw(){
-        c.save()
-        c.beginPath()
-        c.arc(this.x, this.y, this.radius, 0, Math.PI *2,false)
-        c.fillStyle = this.color
-        c.fill()
-        c.restore()
-    }
+const canvas = document.querySelector("canvas");
+let scoreBoard = document.querySelector(".score");
+let start = document.querySelector(".restart");
+let gameover = document.querySelector(".pop");
+let gameOverBtn = document.querySelector(".over");
+let startover = document.querySelector(".start");
+let startGAmebtn = document.querySelector(".startgame");
+const c = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-}
-
-class Projectiles{
-    constructor(x, y, radius, color, velocity){
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = color
-        this.velocity = velocity
-            }
-            draw(){
-                c.save()
-                c.beginPath()
-                c.arc(this.x, this.y, this.radius, 0, Math.PI *2,false)
-                c.fillStyle = this.color
-                c.fill()
-                c.restore()
-            }
-            update(){
-                this.draw()
-                this.x +=this.velocity.x
-this.y +=this.velocity.y
-            }
-}
-
-class Enemies{
-    constructor(x, y, radius, color, velocity){
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = color
-        this.velocity = velocity
-            }
-            draw(){
-                c.save()
-                c.beginPath()
-                c.arc(this.x, this.y, this.radius, 0, Math.PI *2,false)
-                c.fillStyle = this.color
-                c.fill()
-                c.restore()
-            }
-            update(){
-                this.draw()
-                this.x +=this.velocity.x
-this.y +=this.velocity.y
-            }
-}
-
-let projectileArray = [];
-let player;
-let enemiesArray = []
-let enemy;
-let x, y, particles = []
-
-const initialiseEnemies= ()=>{
-    let radius = Math.random() * (20 - 5) + 5
-    if(Math.random() > 0.5){
-        x = x > 0.5 ? 0 - radius:canvas.width / 2 + radius
-        y = Math.random() * canvas.height
-    }
-    if(Math.random() > 0.5){
-        y = y > 0.5 ? 0 - radius:canvas.width / 2 + radius
-        x =Math.random() * canvas.width 
-    }
-    let color = `hsl(${Math.random() * 360}, 50%, 50%)`
-    let angle = Math.atan2(canvas.height/ 2 - y, canvas.width / 2 -  x)
-    let velocity = {
-        x : Math.cos(angle),
-        y : Math.sin(angle)
-    }
-    enemiesArray.push( new Enemies(x, y, radius, color, velocity))
-    
-    // enemy.update()
-
-}
-// setInterval(initialiseEnemies, 1000)
-// initialiseEnemies()
-
-
-const initialisePlayer = ()=>{
-    let x = innerWidth / 2
-    let y = innerHeight / 2
-    let radius = 20
-    let color = 'white'
-    player = new Player(x, y, radius, color)
-    
-    player.draw()
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
 };
+addEventListener("mousemove", (e) => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
 
-function shootOnClick(){
-   
-    canvas.addEventListener('click', (event)=>{
-        let x = canvas.width / 2
-    let y = canvas.height / 2
-    let color = 'white'
-    const angle = Math.atan2(event.clientY - y, event.clientX - x)
+window.addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  // init()
+});
+
+class JohnSnow {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, 30, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+}
+
+class LawnClawSword {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, 10, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+  update() {
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.draw();
+  }
+}
+class ArmyOfTheDead {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+  update() {
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.draw();
+  }
+}
+
+class ParticleExplosion {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+    this.friction = 0.99;
+  }
+  draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+  }
+  update() {
+    this.velocity.x *= this.friction;
+    this.velocity.y *= this.friction;
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.alpha -= 0.02;
+    this.draw();
+  }
+}
+
+let enemies = [],
+  player,
+  x,
+  y,
+  particles = [],
+  score = 0,
+  ammo = [],
+  myScore = [],
+  startgame = false;
+
+function initThePlayer() {
+  let radius = 60;
+  let x = innerWidth / 2;
+  let y = innerHeight / 2;
+  let color = "white";
+
+  player = new JohnSnow(x, y, radius, color);
+}
+
+function createTheEnemy() {
+  let radius = Math.random() * (30 - 8) + 8;
+
+  if (Math.random() > 0.5) {
+    x = Math.random() > 0.5 ? 0 - radius : canvas.width + radius;
+    y = Math.random() * canvas.height;
+  }
+  if (Math.random() > 0.5) {
+    y = Math.random() > 0.5 ? 0 - radius : canvas.height + radius;
+    x = Math.random() * canvas.width;
+  }
+
+  let color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+
+  const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+  let velocity = { x: Math.cos(angle), y: Math.sin(angle) };
+  enemies.push(new ArmyOfTheDead(x, y, radius, color, velocity));
+}
+
+// console.log(gsap)
+function restartOrEndAfterGameover() {
+  addEventListener("click", (e) => {
+    if (e.target.className == "yes") {
+      if (startgame == false) {
+        startgame = true;
+        start.style.display = "none";
+        gameover.style.display = "none";
+        restart();
+      }
+    } else if (e.target.className == "no") {
+      startgame = false;
+
+      if (startgame == false) {
+        start.style.display = "none";
+        startGAmebtn.style.display = "none";
+        gameover.style.display = "none";
+        startover.style.display = "flex";
+      }
+    }
+  });
+}
+restartOrEndAfterGameover();
+
+function calculatePlayerDistanceFromEnemies() {
+  // Invoked in amimate
+
+  enemies.forEach((enemy) => {
+    enemy.update();
+    let playerDistanceFromEnemies = Math.hypot(
+      player.x - enemy.x,
+      player.y - enemy.y
+    );
+
+    if (playerDistanceFromEnemies + enemy.radius < player.radius) {
+      let finalscore = document.querySelector(".finalscore");
+      let gameOverBtn = document.querySelector(".over");
+      gameover.style.display = "flex";
+      cancelAnimationFrame(animationframe); // how to pause the game or stop the animation frame
+      finalscore.textContent = score;
+      myScore.push(score);
+      startgame = false;
+    }
+
+    bulletHitAndExplosion(enemy);
+  });
+}
+
+function bulletHitAndExplosion(enemy) {
+  // invoked in calculate player distance form enemies
+  ammo.forEach((bullet) => {
+    let bulletDistanceFromEnemies = Math.hypot(
+      bullet.x - enemy.x,
+      bullet.y - enemy.y
+    );
+
+    if (bulletDistanceFromEnemies - bullet.radius < enemy.radius) {
+      for (let i = 0; i < enemy.radius * 2; i++) {
+        let x = bullet.x;
+        let y = bullet.y;
+        let radius = Math.random() * 3;
+        let color = enemy.color;
+        let velocity = {
+          x: (Math.random() - 0.5) * (Math.random() * 8),
+          y: (Math.random() - 0.5) * (Math.random() * 8),
+        };
+
+        let explosion = new ParticleExplosion(x, y, radius, color, velocity);
+        particles.push(explosion);
+      }
+
+      //shrink enemy
+      if (enemy.radius - 5 > 10) {
+        gsap.to(enemy, {
+          radius: enemy.radius - 10,
+        });
+        score += 10;
+      } else {
+        // if(enemy.radius < = 5){
+        score += 100;
+        let index = enemies.indexOf(enemy);
+        enemies.splice(index, 1);
+      }
+      scoreBoard.textContent = score;
+    }
+  });
+}
+
+function removeParticles() {
+  particles.forEach((particle) => {
+    if (particle.alpha <= 0) {
+      let index = particles.indexOf(particle);
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+  });
+}
+
+let animationframe;
+
+function animate() {
+  animationframe = requestAnimationFrame(animate);
+  c.fillStyle = "rgba(0, 0, 0, 0.1)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+
+  removeParticles();
+  calculatePlayerDistanceFromEnemies();
+
+  ammo.forEach((bullet) => {
+    bullet.update();
+
+    ClearMissedEnemiesAndBullets();
+  });
+}
+
+function shootOnClick() {
+  addEventListener("click", (e) => {
+    let radius = 3;
+    let x = canvas.width / 2;
+    let y = canvas.height / 2;
+    let color = "white";
+    const angle = Math.atan2(e.clientY - y, e.clientX - x);
     let velocity = {
-        x:Math.cos(angle) * 8,
-        y :Math.sin(angle) * 8
+      x: Math.cos(angle) * 8,
+      y: Math.sin(angle) * 8,
+    };
+    let bullet = new LawnClawSword(x, y, radius, color, velocity);
+    ammo.push(bullet);
+  });
+}
+
+function ClearMissedEnemiesAndBullets() {
+  ammo.forEach((bullet) => {
+    if (
+      bullet.x + bullet.radius > canvas.width ||
+      bullet.x - bullet.radius < 0 ||
+      bullet.y + bullet.radius > canvas.height ||
+      bullet.y - bullet.radius < 0
+    ) {
+      let bulletIndex = ammo.indexOf(bullet);
+      ammo.splice(bulletIndex, 1);
+      // console.log(ammo)
     }
-        projectileArray.push(new Projectiles(x, y, 4, color, velocity))
-      
-        })
-        projectileArray.forEach(projectile =>{
-            projectile.update()
-            if(projectile.x > innerWidth || projectile.y > innerHeight){
-                let index = projectileArray.indexOf(projectile)
-                projectileArray.splice(index, 1)
-                // conso
-                console.log(projectileArray)
-            }
-        })
-      
-}
- 
-const distanceFromEnemyAndProjectile = (enemy)=>{
-    projectileArray.forEach(projectile=>{
-        let distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
-        if(distance - projectile.radius < enemy.radius){
-            console.log('lhi')
-//         for(let i = 0; i < enemy.radius ; i++){
-// let x = projectile.x
-// let y = projectile.y
-// let radius = Math.random() * 3
-// let color = enemy.color
-// let velocity = {
-//     x: (Math.random() - 0.5) * (Math.random() * 8),
-//     y: (Math.random() - 0.5) * (Math.random() * 8)
-// }
-//            particles.push( new Projectiles(x, y, radius,color, velocity ))
-//            particles.forEach(particle=>{
-//                particle.update()
-//            })
-//         }
-
-        }
-
-
-    })
+  });
 }
 
-function playerAndEnemyDistance (enemy){
-    let playerDistanceFromEnemies = Math.hypot(player.x - enemy.x, player.y - enemy.y)
-    if(playerDistanceFromEnemies + enemy.radius < player.radius){
-        console.log('jj')
-    }
+function restart() {
+  if (startgame == false) {
+    startgame = true;
 
+    setInterval(createTheEnemy, 1000);
+  } else {
+    setInterval(createTheEnemy, 1000);
+  }
+  enemies = [];
+  particles = [];
+  score = 0;
+  ammo = [];
+  myScore = [];
+  initThePlayer();
+  requestAnimationFrame(animate);
+  ClearMissedEnemiesAndBullets();
 }
 
-function animate(){
-requestAnimationFrame(animate)
-c.fillRect(0, 0, canvas.width, canvas.height)
-c.fillStyle ='rgba(0, 0, 0, 0.1)'
-
-initialisePlayer()
-shootOnClick()
-enemiesArray.forEach(enemy =>{
-    enemy.update()
-    distanceFromEnemyAndProjectile(enemy)
-    playerAndEnemyDistance(enemy)
-
-})
-
-
+function reset() {
+  if (startgame == false) {
+    addEventListener("load", () => {
+      startover.style.display = "flex";
+    });
+  }
 }
-  animate()   
+
+startGAmebtn.addEventListener("click", () => {
+  startGAmebtn.style.display = "none";
+  restart();
+});
+
+gameOverBtn.addEventListener("click", () => {
+  start.style.display = "flex";
+});
+
+shootOnClick();
+reset();
